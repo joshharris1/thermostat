3c680d06fcb8caf5665a729b9964e8dcc294f551
joshharris1 <josh.harris@cgi.com>
2018-11-08 15:11:06 +0000

    updated

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,45 @@
+// spec/thermostatSpec.js
+
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch PSM off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch PSM back on', function() {
+  	thermostat.switchPowerSavingModeOff();
+  	expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  	thermostat.switchPowerSavingModeOn();
+  	expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+});
